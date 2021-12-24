042132f9382dfa08cdcf218d9c149cc06ced58f1
Instein98 <instein98@gmail.com>
2021-12-23 21:51:17 -0600

    fix Repository for some Closure that have no caja-r4314.jar

--- fixProjects.sh
+++ fixProjects.sh
@@ -1,14 +1,14 @@
 pluginForMockito="  <plugin>\n    <groupId>org.codehaus.mojo</groupId>\n    <artifactId>build-helper-maven-plugin</artifactId>\n    <version>3.2.0</version>\n    <executions>\n        <execution>\n        <id>add-source</id>\n        <phase>generate-sources</phase>\n        <goals>\n          <goal>add-source</goal>\n        </goals>\n        <configuration>\n          <sources>\n            <source>mockmaker/bytebuddy/main/java</source>\n          </sources>\n        </configuration>\n      </execution>\n      <execution>\n        <id>add-test-source</id>\n        <phase>generate-test-sources</phase>\n        <goals>\n          <goal>add-test-source</goal>\n        </goals>\n        <configuration>\n          <sources>\n            <source>mockmaker/bytebuddy/test/java</source>\n          </sources>\n        </configuration>\n      </execution>\n    </executions>\n  </plugin>"
 
+# add caja to Repository
+mkdir -p Repository/caja/caja/r4314/
+cp Projects/Closure/1/lib/caja-r4314.jar Repository/caja/caja/r4314/
+
+# fix pom.xml for Mockito
 pwd=`pwd`
 for proj in `ls Projects`; do
     for idx in `ls Projects/"$proj"`; do
-        if [ "$proj" = 'Closure' ]; then
-            echo Fixing $proj-$idx
-            cd Projects/"$proj"/$idx
-            sed -i 's_<version>r4314</version>_&\n      <scope>system</scope>\n      <systemPath>${project.basedir}/lib/caja-r4314.jar</systemPath>_g' pom.xml
-            cd $pwd
-        elif [ "$proj" = 'Mockito' ]; then
+        if [ "$proj" = 'Mockito' ]; then
             if [[ $idx -ne 1 && $idx -ne 3 && $idx -ne 18 && $idx -ne 19 && $idx -ne 38 ]]; then
                 continue
             fi
