ac733baccac6e3bb53df31fc37d4d2727813daf2
Instein98 <instein98@gmail.com>
2021-12-26 01:45:48 -0600

    handle initialization error

--- check_tests.sh
+++ check_tests.sh
@@ -1,6 +1,27 @@
 cp -r Repository/* ~/.m2/repository/
 pwd=`pwd`
 
+check_has_fail(){
+    if ! grep -q FAIL $1; then
+        echo Error: No failed test in $1!!!
+    fi
+}
+
+check_empty_and_delete(){
+    if [ ! -s $1 ]; then
+        echo Error: $1 is empty!!!
+        rm $1
+    fi
+}
+
+file_contains_string(){
+    if grep -q $2 $1; then
+        true
+    else 
+        false
+    fi
+}
+
 parse_mvn_test_list(){
     find -name "TEST-*.xml" -exec grep testcase {} + | sed -n "s/.*testcase name=\"\(.*\)\" classname=\"\(.*\)\" time=\"\(.*\)\".*/\2#\1 PASS/p" | sort | uniq > mvnTestList
     # different version of surefire may have different format
@@ -12,26 +33,32 @@ parse_mvn_test_list(){
     fi
 }
 
-file_contains_string(){
-    file=$1
-    str=$2
-    if grep -q $str $file; then
-        true
-    else 
-        false
+parse_mvn_failed_tests(){
+    cat mvn-test.log | sed -n 's/\(.*\)(\(.*\))  Time elapsed: .* sec  <<< \(FAILURE\|ERROR\)!/\2#\1/p' > mvnFailedTests
+    if [ ! -s mvnFailedTests ];then
+         sed -n 's/  \(.*\)(\(.*\))/\2#\1/p' mvn-test.log > mvnFailedTests
     fi
+    # match for Mockito-38, it seems to use a different maven version 
+    if [ ! -s mvnFailedTests ];then
+         sed -n 's/\[ERROR\] \(.*\)(\(.*\))  Time elapsed: .* s  <<< \(FAILURE\|ERROR\)!/\2#\1/p' mvn-test.log > mvnFailedTests
+    fi
+    for line in `cat mvnFailedTests`; do
+         sed -i "s/$line PASS/$line FAIL/" mvnTestList
+    done
 }
 
-function check_has_fail(){
-    if ! grep -q FAIL $1; then
-        echo Error: No failed test in $1!!!
-    fi
-}
-
-function check_empty_and_delete(){
-    if [ ! -s $1 ]; then
-        echo Error: $1 is empty!!!
-        rm $1
+# in case some tests get initializationError
+handle_mvn_initError(){
+    echo -n > mvnInitErrorTests
+    sed -n 's/initializationError(\(.*\))  Time elapsed: .* sec  <<< \(FAILURE\|ERROR\)!/\1/p' mvn-test.log | sort | uniq >> mvnInitErrorTests
+    for line in `cat mvnInitErrorTests`;do
+        sed -i "s/$line#initializationError FAIL/$line INITIALIZATION_ERROR/" mvnTestList
+    done
+    if [ ! -s mvnInitErrorTests ];then
+        sed -n 's/.*) initializationError(\(.*\))/\1/p' mvn-test.log | sort | uniq >> mvnInitErrorTests
+        for line in `cat mvnInitErrorTests`;do
+            echo $line INITIALIZATION_ERROR >> mvnTestList
+        done
     fi
 }
 
@@ -70,18 +97,12 @@ for proj in `ls Projects`; do
             # collect mvn test list
             echo Collecting maven test list...
             cd Projects/$proj/$idx  
-            if [ ! -f mvn-test.log ] ||  file_contains_string mvn-test.log "java.lang.OutOfMemoryError:"; then
+            if [ ! -f mvn-test.log ] || [[ ! $(find -name "TEST-*.xml")  ]]  ||  file_contains_string mvn-test.log "java.lang.OutOfMemoryError:"; then
                 mvn -Dhttps.protocols=TLSv1.2 -DargLine="-Xmx4096m"  clean test -l mvn-test.log
             fi
             parse_mvn_test_list
-            cat mvn-test.log | sed -n 's/\(.*\)(\(.*\))  Time elapsed: .* sec  <<< \(FAILURE\|ERROR\)!/\2#\1/p' > mvnFailedTests
-            if [ ! -s mvnFailedTests ];then
-               sed -n 's/  \(.*\)(\(.*\))/\2#\1/p' mvn-test.log > mvnFailedTests
-            fi
-            # Todo: add match for Mockito-38 mvn-test.log, it seems to use a different maven version 
-            for line in `cat mvnFailedTests`; do
-                sed -i "s/$line PASS/$line FAIL/" mvnTestList
-            done
+            parse_mvn_failed_tests
+            handle_mvn_initError
             cd $pwd && cp Projects/$proj/$idx/mvnTestList TestLists/$proj/$idx/mvnTestList
             check_has_fail TestLists/$proj/$idx/mvnTestList
         else
